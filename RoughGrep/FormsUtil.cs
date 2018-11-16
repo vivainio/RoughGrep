e6702e93a0b700a4826515f17b1fa54f7e4b40f9
Ville M. Vainio <ville.vainio@basware.com>
2018-11-16 20:38:16 +0200

    better discovery for new rectangle

--- RoughGrep/FormsUtil.cs
+++ RoughGrep/FormsUtil.cs
@@ -17,8 +17,13 @@ namespace RoughGrep
             var right = Rectangle.FromLTRB(originalForm.Right, 0, workarea.Right,workarea.Bottom);
             var top = Rectangle.FromLTRB(0, 0, workarea.Right, originalForm.Top);
             var bottom = Rectangle.FromLTRB(0, originalForm.Bottom, workarea.Right, workarea.Bottom);
-            var all = new[] { left, right, top, bottom };
-            var best = all.OrderByDescending(r => r.Width * r.Height).First();
+            var all = new[] { left, right, top, bottom }.Where(r => r.Height > 200 && r.Width > 200);
+            var best = all.OrderByDescending(r => r.Width * r.Height).FirstOrDefault();
+            if (best == default(Rectangle))
+            {
+                best = Rectangle.FromLTRB(workarea.Right / 2, 0, workarea.Right, workarea.Bottom);
+            }
+            
             newForm.Location = best.Location;
             newForm.Size = best.Size;
         }
